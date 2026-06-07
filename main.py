0e6695b30777d0920d2ca0b3fdf434a6f2b98831
Arjan Khandelwal <arjankhandelwal394@gmail.com>
2026-06-07 22:40:33 +0530

    fix CORS for Render deployment

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://convert-r-web.vercel.app"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
